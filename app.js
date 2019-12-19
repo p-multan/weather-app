f73eba037c209576155042010baa264f501363d7
Pawel Multan <pj.multan@gmail.com>
2019-12-19 23:27:08 +0100

    fix api problem by using cors-anywhere proxy

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ window.addEventListener('load', () => {
       long = position.coords.longitude;
 
       // Some problem with the api, to use it we have to use proxy
-      const proxy = `http://cors-anywhere.herokuapp.com/`;
+      const proxy = `https://cors-anywhere.herokuapp.com/`;
       const api = `${proxy}https://api.darksky.net/forecast/18013269f886ca9c1a015f535bdd9a40/${lat},${long}`;
 
       fetch(api)
